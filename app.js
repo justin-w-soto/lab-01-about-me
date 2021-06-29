f3b442151aba3ea7959ac97165b9e79535fe0a4b
Justin W Soto <soto@pdx.edu>
2021-06-29 14:20:21 -0700

    cleaned up html

--- app.js
+++ app.js
@@ -8,34 +8,37 @@
 // track num of correct answers
 // at quiz end, update results section with number of correct answers from quiz.
 
-// const quizButton = document.getElementById('click-me');
-// const results = document.getElementById('results');
+const quizButton = document.getElementById('click-me');
+const results = document.getElementById('results');
 
-// quizButton.addEventListener('click', ()=>{
-//     const confirmed = confirm('Would you like to start the quiz?');
-//     console.log(confirmed);
-//     if (confirmed) {
-//         const name = prompt('What is your name?');
-//         const ans1 = prompt('Q1');
-//         const ans2 = prompt('Q2');
-//         const ans3 = prompt('Q3');
-//         const ans4 = prompt('Q4');
-//         console.log(name, ans1, ans2, ans3, ans4);
-//         let score = 0;
-//         if (ans1 === 'yes') {
-//             score ++;
-//         }
-//         if (ans2 === 'yes') {
-//             score ++;
-//         }
-//         if (ans3 === 'yes') {
-//             score ++;
-//         }
-//         if (ans4 === 'yes') {
-//             score ++;
-//         }
-//         else {
-//             console.log('user cancelled');
-//         }
-//     }
-// });
+quizButton.addEventListener('click', ()=>{
+    const confirmed = confirm('Would you like to start the quiz?');
+    console.log(confirmed);
+    if (confirmed) {
+        const name = prompt('What is your name?');
+        const ans1 = prompt('Is Michelangelos favorite food pizza?');
+        const ans2 = prompt('Q2');
+        const ans3 = prompt('Q3');
+        const ans4 = prompt('Q4');
+        console.log(name, ans1, ans2, ans3, ans4);
+
+        let score = 0;
+
+        if (ans1 === 'yes') {
+            score ++;
+        }
+        if (ans2 === 'yes') {
+            score ++;
+        }
+        if (ans3 === 'yes') {
+            score ++;
+        }
+        if (ans4 === 'yes') {
+            score ++;
+        }
+        results.textContent = 'You got ${score} right';
+        else {
+            console.log('user cancelled');
+        }
+    }
+});
---
+++ test/utils.js
@@ -0,0 +1,9 @@
+export function isYes(word) {
+    word = word.toLowerCase();
+
+    if (word === 'yes' || word === 'y'){
+        return true;
+    } else {
+        false;
+    }
+}
